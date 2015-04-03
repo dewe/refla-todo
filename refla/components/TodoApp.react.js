d035df4f5403cf3c102a94e01e0a4e9b4d63b2f2
Johan Dewe <johan.dewe@gavagai.se>
2015-04-03 21:08:19 +0200

    naive todolist

--- refla/components/TodoApp.react.js
+++ refla/components/TodoApp.react.js
@@ -1,7 +1,33 @@
 var React = require('react');
+var TodoList = require('./TodoList.react');
 
-module.exports = React.createClass({
-    render: function() {
-        return React.DOM.div({ className:'todo-container' }, 'react todo app');
-    }
+module.exports = TodoApp = React.createClass({
+
+  getInitialState: function() {
+    return {items: [], text: ''};
+  },
+
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  },
+
+  render: function() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.onChange} value={this.state.text} />
+          <button>{'Add #' + (this.state.items.length + 1)}</button>
+        </form>
+        <TodoList items={this.state.items} />
+      </div>
+    );
+  }
 });
